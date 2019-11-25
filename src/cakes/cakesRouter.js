fe71def902da0c9f3551c38506e1930596dd2b61
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-24 20:31:38 -0500

    put endpoint done

--- src/cakes/cakesRouter.js
+++ src/cakes/cakesRouter.js
@@ -52,6 +52,40 @@ cakesRouter
 		res.json(cakes)
 		next()
 	})
+
+	//  PUT /cakes/{id}
+	.put((req, res, next) => {
+		const { id, name, comment, imageUrl, yumFactor } = req.body        
+		const cakeToUpdate = { id, name, comment, imageUrl, yumFactor }
+		
+		const numberOfValues = Object.values(cakeToUpdate).filter(Boolean).length
+            
+		if (numberOfValues === 0) {
+			return res.status(400).json({
+				error: {
+					message: `Request body must contain either name, comment, or yumFactor`
+				}
+			})
+		}
+
+		for (let cake of SampleCakes) {
+			if (cake.id == id) {
+				if (cakeToUpdate.name != null || undefined) {
+					cake.name = cakeToUpdate.name;
+				}
+				if (cakeToUpdate.comment != null || undefined) {
+					cake.comment = cakeToUpdate.comment;
+				}
+				if (cakeToUpdate.yumFactor != null || undefined) {
+					cake.yumFactor = cakeToUpdate.yumFactor;
+				}
+			}
+		}	
+		console.log(SampleCakes)
+		res
+      .status(200)
+      .json(SampleCakes)       
+	})  
 	
 	
 module.exports = cakesRouter
