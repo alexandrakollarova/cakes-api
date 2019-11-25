924c9140f6b66f72766acc5bbf0e237b1f7e2554
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-24 19:57:05 -0500

    get by id endpoint done

--- src/cakes/cakesRouter.js
+++ src/cakes/cakesRouter.js
@@ -8,7 +8,23 @@ cakesRouter
   .route('/')
   .get((req, res) => {    
     res.json(SampleCakes)
-  })
+	})
+	
+//  GET /cakes/{id}   
+cakesRouter
+  .route('/:cake_id')
+	.get((req, res, next) => {
+		let { cake_id } = req.params;
+		let cake = SampleCakes.find(cake => cake.id == cake_id);		
+
+		if (!cake_id) {
+			return res.status(404).json({
+				error: { message: `Cake with id ${cake_id} doesn't exist` }
+			})
+		}
+		res.json(cake) 
+		next()
+	})
 	
 	
 module.exports = cakesRouter
